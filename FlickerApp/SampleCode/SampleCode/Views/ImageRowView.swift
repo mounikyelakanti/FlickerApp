05261d6754dcedfa32005f757c934060e1958f47
mounikyelakanti <yelakanti94@gmail.com>
2024-06-27 08:17:45 -0700

    Add files via upload

---
+++ FlickerApp/SampleCode/SampleCode/Views/ImageRowView.swift
@@ -0,0 +1,35 @@
+//
+//  ImageRowView.swift
+//  SampleCode
+//
+//  Created by Mounik Yelakanti on 2/19/24.
+//
+
+import SwiftUI
+
+struct ImageRowView: View {
+    let image: FlickrImage
+
+    var body: some View {
+        HStack {
+            AsyncImage(url: URL(string: image.media.m)) { phase in
+                switch phase {
+                case .success(let image):
+                    image.resizable()
+                case .failure:
+                    Text("Failed to load image" + image.media.m)
+                case .empty:
+                    ProgressView()
+                @unknown default:
+                    EmptyView()
+                }
+            }
+            .frame(width:  100, height:  100)
+            VStack(alignment: .leading) {
+                Text(image.title)
+                    .font(.headline)
+                    .font(.subheadline)
+            }
+        }
+    }
+}
